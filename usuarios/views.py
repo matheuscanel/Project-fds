1180902ac42d5059363a391165f051f3b2508bd9
Matheus Canel <matheusxcanel@gmail.com>
2023-04-11 23:01:50 -0300

    Login e cadastro (falta conectar os dois)

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def cadastro(request):
-    if request.method == "GET":
         return render(request, 'cadastro.html')
+
+def login(request):
+        return render(request, 'login.html')
     
+
     
