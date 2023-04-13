425d89f770984adafeccdf094785de5697d8cd15
Lucas Emery <lustosal568@gmail.com>
2023-04-13 14:24:31 -0300

    classe produto criada

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
-from .models import Produto
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao = models.TextField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    imagem = models.ImageField(upload_to='produtos/', null=True, blank=True)
+    estoque = models.PositiveIntegerField()
+
 
 class Carrinho(models.Model):
     usuario = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -9,4 +16,8 @@ class ItemCarrinho(models.Model):
     produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
     quantidade = models.PositiveIntegerField(default=1)
     carrinho = models.ForeignKey(Carrinho, on_delete=models.CASCADE)
+
+
+
+
 
