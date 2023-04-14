6c2ac269c222c1182ed8103f6827845254482dd8
Lucas Emery <lustosal568@gmail.com>
2023-04-14 09:19:11 -0300

    correçao do codigo evoluido

--- CamisasNE/settings.py
+++ CamisasNE/settings.py
@@ -38,7 +38,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'seuapp.middleware.CarrinhoMiddleware',
 ]
 
 ROOT_URLCONF = 'CamisasNE.urls'
