1a84d0df2e94ef0ad559427af2eba2019ea9ae35
Lucas Emery <lustosal568@gmail.com>
2023-04-15 12:09:23 -0300

    http gerado jo meu com sucesso

--- CamisasNE/urls.py
+++ CamisasNE/urls.py
@@ -1,10 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path, include 
+from usuarios import views as usuarios_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('usuarios/', include('usuarios.urls'))
-    
+    path('usuarios/', include('usuarios.urls')),
+    path('', usuarios_views.home, name='home'),
+
     
 ]
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
 from . import views
+from django.urls import path, include
+
 
 urlpatterns = [
     path('cadastro/', views.cadastro, name = "cadastro"),
     path('login/', views.login, name = "login"),
-    path('home/', views.home, name="home"),
+    path('', views.home, name="home"),
     path('adicionar_ao_carrinho/<int:produto_id>/', views.adicionar_ao_carrinho, name='adicionar_ao_carrinho'),
     path('confirmar_compra/', views.confirmar_compra, name = "confirmar_compra"),
     path('team_selection/', views.team_selection, name="team_selection"),
 
 
 ]
+
 
