417dadead1b6f1faf83a31b95b01d4b689b8dd9b
Antonio Valença <antoniotvalenca@gmail.com>
2023-06-04 12:49:35 -0300

    carrinho funcionando

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 urlpatterns = [
     path('cadastro/', views.cadastro, name="cadastro"),
-    path('login/', views.login, name="login"),
+    path('carrinho/remover/<int:item_carrinho_id>/', views.remover_item_carrinho, name='remover_item'),
+    path('login/', views.login_view, name="login"),
     path('home/', views.home, name="home"),
     path('team_selection/', views.team_selection, name="team_selection"),
     path('', views.inicial, name="inicial"),
-    path('carrinho/<int:produto_id>/', views.adicionar_ao_carrinho, name='adicionar_ao_carrinho'),
-    path('carrinho/', views.exibir_carrinho, name='exibir_carrinho'),
+    path('carrinho/', views.carrinho, name='carrinho'),
     path('avaliar/', views.avaliar, name='avaliar'),
     path('confirmar_compra/', views.confirmar_compra, name='confirmar_compra'),
     path('carrinho/finalizar/', views.finalizar_compra, name='finalizar_compra'),
